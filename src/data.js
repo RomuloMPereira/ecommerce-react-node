1dc3595db69c594d1105abcb7c4b1aeecfbd7a14
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-01-11 19:13:53 -0300

    Slider (Pure React)

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "http://lornaluxe.com/wp-content/uploads/2015/04/julia-bergshoeff-by-glen-luchford-for-vogue-paris-may-2015-3.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "#f9f8f6",
+    },
+    {
+        id: 2,
+        img: "https://hips.hearstapps.com/hbz.h-cdn.co/assets/15/46/hbz-coziest-editorials-bazaar-09_2.jpg",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "#f6f7fb",
+    },
+    {
+        id: 3,
+        img: "https://danielaburity.files.wordpress.com/2013/04/maryna-linchuk-by-lacey-playing-with-color-vogue-japan-march-2013-3.jpg",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "#feebe7",
+    },
+];
