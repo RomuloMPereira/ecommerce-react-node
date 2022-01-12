ce063336300074f8a14f235deafe01c903e63df0
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-01-12 15:41:12 -0300

    Hover and animation on the Product Card

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,6 +4,22 @@ import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
+const Info = styled.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0,0,0,0.1);
+    z-index: 3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    opacity: 0;
+    transition: all 0.5s ease;
+    cursor: pointer;
+`
+
 const Container = styled.div`
     flex: 1;
     margin: 5px;
@@ -14,6 +30,10 @@ const Container = styled.div`
     align-items: center;
     background-color: #f5fbfd;
     position: relative;
+
+    &:hover ${Info}{
+        opacity: 1;
+    }
 `
 
 const Circle = styled.div`
@@ -27,19 +47,6 @@ const Circle = styled.div`
 const Image = styled.img`
     height: 75%;
     z-index: 2;
-`
-
-const Info = styled.div`
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background-color: rgba(0,0,0,0.1);
-    z-index: 3;
-    display: flex;
-    align-items: center;
-    justify-content: center;
 `
 
 const Icon = styled.div`
