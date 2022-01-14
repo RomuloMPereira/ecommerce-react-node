76838f0a37faf2e48920b145fe36fa2fba552e2e
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-01-14 14:33:29 -0300

    Responsive Product Page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -13,6 +13,10 @@ const Container = styled.div`
 const Wrapper = styled.div`
     padding: 50px;
     display: flex;
+    @media only screen and (max-width: 380px) {
+        padding: 10px;
+        flex-direction: column;
+    }
 `
 
 const ImageContainer = styled.div`
@@ -23,11 +27,17 @@ const Image = styled.img`
     width: 100%;
     height: 90vh;
     object-fit: cover;
+    @media only screen and (max-width: 380px) {
+        height: 40vh;
+    }
 `
 
 const InfoContainer = styled.div`
     flex: 1;
     padding: 0px 50px;
+    @media only screen and (max-width: 380px) {
+        padding: 10px;
+    }
 `
 
 const Title = styled.h1`
@@ -48,6 +58,9 @@ const FilterContainer = styled.div`
     margin: 30px 0px;
     display: flex;
     justify-content: space-between;
+    @media only screen and (max-width: 380px) {
+        width: 100%;
+    }
 `
 
 const Filter = styled.div`
@@ -83,6 +96,9 @@ const AddContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    @media only screen and (max-width: 380px) {
+        width: 100%;
+    }
 `
 
 const AmountContainer = styled.div`
